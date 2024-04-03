cf735d2afcd51bb3e9d35fca079c3a7102a9530d
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-04-03 16:35:15 +0900

    Title: 진수 to 진수, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240403/진수 to 진수/transformation-of-number-system.py
@@ -0,0 +1,18 @@
+a, b = map(int,input().split())
+n=input()
+
+num=0
+for i in n:
+    num=num*a+int(i)
+
+digits=[]
+while True:
+    if num<b:
+        digits.append(num)
+        break
+    
+    digits.append(num%b)
+    num //= b
+
+for i in digits[::-1]:
+    print(i,end='')
