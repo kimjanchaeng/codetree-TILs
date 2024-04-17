17da81c285bd1921f6a0f5eb06ef83bb42983143
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-04-17 13:45:03 +0900

    Title: 최소공배수 구하기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240417/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240417/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,9 +1,13 @@
+def print_gcd(n,m):
+    a=1
+    for i in range(1,min(n,m)+1):
+        if n%i==0 and m%i==0:
+            a*=i
+    
+    print(a)
+
+
+
+
 n, m= list(map(int,input().split()))
-
-a=1
-for i in range(1,min(n,m)+1):
-    if n%i==0 and m%i==0:
-        a*=i
-
-
-print(a)
+print_gcd(n,m)
