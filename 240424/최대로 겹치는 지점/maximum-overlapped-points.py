24e4b66b4d26afec54227d89c531273b6a7310eb
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-04-24 22:26:25 +0900

    Title: 최대로 겹치는 지점, Time: 139ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240424/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -0,0 +1,14 @@
+n=int(input())
+
+block=[0]*100
+for i in range(n):
+    a, b = list(map(int,input().split()))
+    for j in range(a,b):
+        block[j]+=1
+
+count=0
+for i in block:
+    if i>1:
+        count+=1
+
+print(count)
