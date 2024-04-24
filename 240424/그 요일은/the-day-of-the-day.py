29b5b11725a720b7aab63363d75f28702fd94601
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-04-24 11:05:30 +0900

    Title: 그 요일은, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240424/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,18 @@
+m1, d1, m2, d2 = list(map(int,input().split()))
+day=input()
+
+days=['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+dates=[0,31,29,31,30,31,30,31,31,30,31,30,31]
+
+s=days.index(day)    #find는 list형태 안됨
+
+day1=sum(dates[:m1])+d1
+day2=sum(dates[:m2])+d2
+
+count=0
+
+d=day2-day1
+count+=d//7
+if d&7<=s:
+    count+=1
+print(count)
