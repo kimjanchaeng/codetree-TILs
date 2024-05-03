95f83d39bd8437802f68ed3867973a23f9c350d5
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-05-03 15:07:42 +0900

    Title: 연속되는 수 2, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240503/연속되는 수 2/continuous-number2.py
+++ 240503/연속되는 수 2/continuous-number2.py
@@ -7,10 +7,12 @@ for i in range(n):
 
 cnt=1
 di=[]
+result=0
 for i in range(len(digit)-1):
     if digit[i]==digit[i+1]:
         cnt+=1
     else:
-        di.append(cnt)    
+        # di.append(cnt)
+        result=max(cnt,result)    
         cnt=1
-print(max(di))
+print(result)
