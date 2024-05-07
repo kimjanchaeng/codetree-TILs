5a1391e9f8b548892d379cd8fdfa76ce09aff31d
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-05-07 17:52:14 +0900

    Title: 연속되는 수 4, Time: 158ms, Memory: 24MB, Status: Passed - CodeTree

--- 240507/연속되는 수 4/continuous-number4.py
+++ 240507/연속되는 수 4/continuous-number4.py
@@ -1,18 +1,14 @@
-n=int(input())
-
-s=[]
-
+n = int(input())
+ans = 0
+cnt = 0
+num = 0
 for i in range(n):
-    a=int(input())
-    s.append(a)
-
-cnt=1
-b=1
-for j in range(len(s)-1):
-    if s[j]<s[j+1]:
-        cnt+=1
+    num_new = int(input())
+    if num_new > num:
+        cnt += 1
     else:
-        if b<cnt:
-            b=cnt
-        cnt=1
-print(b)
+        cnt = 1
+    ans = max(cnt, ans)
+    num = num_new
+
+print(ans)
