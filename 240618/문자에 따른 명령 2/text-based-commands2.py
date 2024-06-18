ac7a8cdf206e764a87a68e4614d69780d9a54336
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-06-18 14:49:45 +0900

    Title: 문자에 따른 명령 2, Time: 167ms, Memory: 25MB, Status: Passed - CodeTree

--- 240618/문자에 따른 명령 2/text-based-commands2.py
+++ 240618/문자에 따른 명령 2/text-based-commands2.py
@@ -1,10 +1,14 @@
 x,y=0,0
 dir=3
 dx,dy=[1,0,-1,0], [0,-1,0,1]
-n=input().split()
-if n[0]=='R':
-    dir=(dir+1)%4
-else:
-    dir=(dir+3)%4
-x,y=x+dx[dir],y+dy[dir]
+n=input()
+for i in range(len(n)):
+    # print(i)
+    if n[i]=='R':
+        dir=(dir+1)%4
+    elif n[i]=='L':
+        dir=(dir+3)%4
+    else:
+        x,y=x+dx[dir],y+dy[dir]
+
 print(x,y)
