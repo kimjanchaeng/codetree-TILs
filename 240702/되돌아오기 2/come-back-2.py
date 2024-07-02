18c6debf6b8b0d65764b896f4ee4afa2a0621c18
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-07-02 21:27:13 +0900

    Title: 되돌아오기 2, Time: 138ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240702/되돌아오기 2/come-back-2.py
@@ -0,0 +1,23 @@
+s=input()
+# print(s)
+
+x,y=0,0
+dx,dy=[0,1,0,-1],[1,0,-1,0]
+dir=3
+
+ans=-1
+cnt=0
+t=0
+for i in s:
+    if i=='F':
+        x,y=x+dx[dir],y+dy[dir]
+        cnt+=1
+    elif i=='L':
+        dir=(dir+3)%4
+    else:
+        dir=(dir+1)%4
+
+    t+=1
+    if x==0 and y==0 and cnt>0:
+        ans=t
+print(ans)
