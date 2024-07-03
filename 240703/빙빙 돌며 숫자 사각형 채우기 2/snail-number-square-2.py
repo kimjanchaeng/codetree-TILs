4c72b0272d3b08e9f058ee0b498e660267550452
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-07-03 14:07:37 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240703/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -0,0 +1,24 @@
+n, m = list(map(int,input().split()))
+
+mat=[[0]*m for i in range(n)]
+# print(mat)
+
+def in_range(x,y):
+    return 0<=x and x<n and 0<=y and y<m
+
+dx,dy=[0,1,0,-1],[1,0,-1,0]
+dir=1
+x,y=0,0
+mat[x][y]=1
+for i in range(2,n*m+1):
+    nx,ny=x+dx[dir],y+dy[dir]
+    # print(nx,ny)
+    if not in_range(nx,ny) or mat[nx][ny]!=0:
+        dir=(dir+3)%4
+    x,y=x+dx[dir],y+dy[dir]
+    mat[x][y]=i
+
+for i in mat:
+    for j in i:
+        print(j,end=' ')
+    print()
