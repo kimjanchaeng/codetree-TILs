15d3b984db1d317b3dec6220313a65a94740295d
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-07-04 20:24:19 +0900

    Title: 체크판위에서 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240704/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,15 @@
+R, C= list(map(int,input().split()))
+
+mat=[list(map(str,input().split())) for i in range(C)]
+# print(mat)
+
+sta=mat[0][0]
+cnt=0
+for i in range(1,R-1):
+    for j in range(1,C-1):
+        if mat[i][j]!=sta:
+            for k in range(i+1,R-1):
+                for l in range(j+1,C-1):
+                    if mat[k][l]!=mat[i][j] and mat[k][l]!=mat[R-1][C-1]:
+                        cnt+=1
+print(cnt)
