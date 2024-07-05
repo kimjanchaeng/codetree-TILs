fc5841b4b8fd13350e9b8e459cfc819f8ef28329
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-07-06 01:21:39 +0900

    Title: 씨 오 더블유 2, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

--- 240706/씨 오 더블유 2/c-o-w-2.py
+++ 240706/씨 오 더블유 2/c-o-w-2.py
@@ -5,11 +5,20 @@ s=input()
 cnt=0
 cnt1=0
 cnt2=0
-for i in s:
-    if i=='C':
-        cnt+=1
-    elif i=='O':
-        cnt1+=1
-    else:
-        cnt2+=1
-print(cnt*cnt1*cnt2)
+
+# for i in s:
+#     # print(i)
+#     if i=='C':
+#         cnt+=1
+#     elif i=='O':
+#         cnt1+=1
+#     else:
+#         cnt2+=1
+# print(cnt*cnt1*cnt2)
+
+for i in range(N):
+    for j in range(i+1,N):
+        for k in range(j+1,N):
+            if s[i]=='C' and s[j]=='O' and s[k]=='W':
+                cnt+=1
+print(cnt)
