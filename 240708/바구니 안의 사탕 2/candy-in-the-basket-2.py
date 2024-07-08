06dab75eb47aaa6f7961b401401b0ac593e0c4cc
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-07-08 21:04:33 +0900

    Title: 바구니 안의 사탕 2, Time: 137ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240708/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,11 @@
+N,K=list(map(int,input().split()))
+
+arr=[0]*101
+for i in range(N):
+    can,loc=list(map(int,input().split()))
+    arr[loc]=can
+
+ans=0
+for i in range(1,101-(2*K+1)+2):
+    ans=max(sum(arr[i:i+2*K+1]),ans)
+print(ans)
