f3d194a0bdf08ccea33cf10ad6b318299d517810
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-07-08 20:33:06 +0900

    Title: 아름다운 수열 2, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240708/아름다운 수열 2/beautiful-sequence-2.py
+++ 240708/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,14 +1,22 @@
 N, M = list(map(int,input().split()))
 A=list(map(int,input().split()))
 B=list(map(int,input().split()))
+# B.sort()
 
 ans=0
 for i in range(N-len(B)+1):
     cnt=0
+    s=[]
     for j in range(i,i+len(B)):
-        if A[j] in B:
+        s.append(A[j])
+    s.sort()
+    B.sort()
+    for i in range(len(B)):
+        if s[i]==B[i]:
             cnt+=1
+
     if cnt==len(B):
         ans+=1
+
 
 print(ans)
