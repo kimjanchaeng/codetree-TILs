81c25fb9cf95c1d0560d0471b1db60d263a7587b
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-07-08 21:06:47 +0900

    Title: 바구니 안의 사탕 2, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240708/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240708/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -3,7 +3,7 @@ N,K=list(map(int,input().split()))
 arr=[0]*101
 for i in range(N):
     can,loc=list(map(int,input().split()))
-    arr[loc]=can
+    arr[loc]+=can
 
 ans=0
 for i in range(1,101-(2*K+1)+2):
