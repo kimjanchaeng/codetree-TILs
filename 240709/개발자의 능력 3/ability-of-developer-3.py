022580b67cb25db1bda55015eaaaf2437b066dc2
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-07-09 22:54:44 +0900

    Title: 개발자의 능력 3, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240709/개발자의 능력 3/ability-of-developer-3.py
+++ 240709/개발자의 능력 3/ability-of-developer-3.py
@@ -1,11 +1,13 @@
+import sys
 arr=list(map(int,input().split()))
+INT_MAX = sys.maxsize
 
 def get_avg(a,b,c):
     sum1=a+b+c
     sum2=sum(arr)-sum1
     return abs(sum1-sum2)
 avg=0
-ans=100000
+ans=INT_MAX
 for i in range(6):
     for j in range(i+1,6):
         for k in range(j+1,6):
