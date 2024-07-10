b8fda9465a1fd3a1b351d1e6fd671191d84f63ad
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-07-10 14:52:01 +0900

    Title: 두 가지로 열리는 자물쇠, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240710/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 240710/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -20,7 +20,7 @@ arr2=list(map(int,input().split()))
 # print(s1)
 # print(s2)
 # for i in s1:
-
+ans=1
 digit=[i for i in range(1,N+1)]
 # print(digit)
 s1=[]
@@ -36,13 +36,18 @@ for j in range(3):
     # s2.append(digit[arr2[j]-3:arr2[j]+2])
     s1.append(a1)
     s2.append(a2)
+    cnt=0
+    for l in range(5):
+        if a1[l] in a2:
+            cnt+=1
+    ans=cnt*ans
 # print(s1)
 # print(s2)
-ans=1
-for i in range(3):
-    cnt=0
-    for j in range(5):
-        if s1[i][j] in s2[i]:
-            cnt+=1
-    ans=ans*cnt
+# ans=1
+# for i in range(3):
+#     cnt=0
+#     for j in range(5):
+#         if s1[i][j] in s2[i]:
+#             cnt+=1
+#     ans=ans*cnt
 print(250-ans)
