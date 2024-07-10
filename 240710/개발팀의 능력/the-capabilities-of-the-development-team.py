8311009a70783b1df74e4c5aba0a879090d4dda9
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-07-10 23:33:37 +0900

    Title: 개발팀의 능력, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240710/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 240710/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -6,6 +6,7 @@ N=5
 INT_MAX=sys.maxsize
 ans=INT_MAX
 t1,t2,t3,s=0,0,0,0
+Flag=False
 for i in range(N):
     for j in range(i+1,N):
         
@@ -17,9 +18,10 @@ for i in range(N):
                 t2=arr[k]+arr[l]
                 t3=sum(arr)-t1-t2
                 if t1!=t2 and t1!=t3 and t2!=t3:
+                    Flag=True
                     s=max(t1,t2,t3)-min(t1,t2,t3)
                     ans=min(ans,s)
-if ans==INT_MAX:
+if Flag==False:
     print(-1)
-
-print(ans)
+else:
+    print(ans)
