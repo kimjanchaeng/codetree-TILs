7ab7a2bd23e4f4cadf952c2753554713882e6335
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-07-15 01:49:54 +0900

    Title: 등차수열, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240715/등차수열/arithmetic-sequence.py
@@ -0,0 +1,14 @@
+n=int(input())
+
+arr=list(map(int,input().split()))
+# print(arr)
+st=min(arr)
+ls=max(arr)
+# print(max(arr))
+cnt=0
+for i in range(n-1):
+    for j in range(i+1,n):
+        for k in range(st,ls+1):
+            if arr[i]-k==k-arr[j]:
+                cnt+=1
+print(cnt)
