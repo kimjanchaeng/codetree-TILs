f3febd79e490fc9da6f61dc9f7923db57ccb5d3a
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-07-14 21:59:58 +0900

    Title: 이상한 폭탄 2, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240714/이상한 폭탄 2/strange-bomb-2.py
+++ 240714/이상한 폭탄 2/strange-bomb-2.py
@@ -1,11 +1,13 @@
 N , K= list(map(int,input().split()))
 cor=[int(input()) for i in range(N)]
 
+# print(input(cor).split())
 ans=-1
 # s=0
-for i in range(N-K+1):
+for i in range(N-1):
     # for j in range(i,i+K):
     # print(cor[i+1:min(i+K+1,N)])
+
     if cor[i] in cor[i+1:min(i+K+1,N)]:
         # print(333)
         ans=max(ans,cor[i])
