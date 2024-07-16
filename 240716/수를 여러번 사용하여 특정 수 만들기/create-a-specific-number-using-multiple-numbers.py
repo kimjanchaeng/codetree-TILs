d4a06bd36ab38e41502b082bc2a477816e59b5df
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-07-16 22:18:41 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240716/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
+++ 240716/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
@@ -1,7 +1,7 @@
 A, B, C= list(map(int,input().split()))
 ans=0
-for i in range(100):
-    for j in range(100):
+for i in range(1000):
+    for j in range(1000):
         sum1=A*i+B*j
         diff=C-sum1
         if diff>=0:
