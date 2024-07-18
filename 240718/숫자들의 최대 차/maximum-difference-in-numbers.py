b758bfb87090c0fb351b322d12adeab2717370f6
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-07-18 23:45:24 +0900

    Title: 숫자들의 최대 차, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240718/숫자들의 최대 차/maximum-difference-in-numbers.py
+++ 240718/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -8,7 +8,7 @@ ans=0
 for i in range(sta,fin+1):
     cnt=0
     for j in range(n):
-        if arr[j]>=i and arr[j]<=i+3:
+        if arr[j]>=i and arr[j]<=i+k:
             cnt+=1
         
     ans=max(cnt,ans)
