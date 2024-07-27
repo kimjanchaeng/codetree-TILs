f4f101841c25700ad63469be472f3060e6781af5
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-07-27 23:16:58 +0900

    Title: 초기 수열 복원하기, Time: 184ms, Memory: 25MB, Status: Passed - CodeTree

--- 240727/초기 수열 복원하기/restore-initial-sequence.py
+++ 240727/초기 수열 복원하기/restore-initial-sequence.py
@@ -1,17 +1,36 @@
-import itertools
+# import itertools
+
+# n=int(input())
+# arr=list(map(int,input().split()))
+# n_arr=[i for i in range(1,n+1)]
+# # print(n_arr)
+
+# arr_las=list(itertools.permutations(n_arr,n))
+
+# for i in arr_las:
+#     s=[]
+#     for j in range(n-1):
+#         s.append(i[j]+i[j+1])
+
+#     if s==arr:
+#         for k in i:
+#             print(k,end=' ')
 
 n=int(input())
-arr=list(map(int,input().split()))
-n_arr=[i for i in range(1,n+1)]
-# print(n_arr)
+array=list(map(int,input().split()))
+answer=[]
 
-arr_las=list(itertools.permutations(n_arr,n))
-
-for i in arr_las:
-    s=[]
+for i in range(n):
+    success=True
+    temp=[0 for _ in range(n)]
+    temp[0]=i+1
     for j in range(n-1):
-        s.append(i[j]+i[j+1])
-
-    if s==arr:
-        for k in i:
-            print(k,end=' ')
+        if abs(array[j]-temp[j]) in temp:
+            success=False
+            break
+        temp[j+1]=abs(array[j]-temp[j])
+    if success:
+        answer.append(temp)
+        
+answer=sorted(answer)
+print(*answer[0])
