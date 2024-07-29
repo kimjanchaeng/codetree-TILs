36a35d721754a1b3b84201b06539f986cf10c2ac
kimjanchaeng <138867361+kimjanchaeng@users.noreply.github.com>
2024-07-30 00:29:57 +0900

    Title: 전부 겹치는 선분, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240730/전부 겹치는 선분/overlapping-line-segments.py
@@ -0,0 +1,17 @@
+n=int(input())
+arr=[list(map(int,input().split())) for i in range(n)]
+# print(arr)
+
+def intersect(a,b,c,d):
+    if d<a or b<c:
+        return False
+    else:
+        return True
+
+for i in range(n):
+    for j in range(i+1,n):
+        x1,x2,x3,x4=arr[i][0],arr[i][1],arr[j][0],arr[j][1]
+        if not intersect(x1,x2,x3,x4):
+            print('No')
+
+print('Yes')
